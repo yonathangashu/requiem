0ad46bd311388add14ba327e0472666c6e6b59c0
Yonathan Gashu <yonathangashu@gmail.com>
2026-04-10 01:48:04 -0400

    Add page allocation, attempt transfer to mac env

--- uefi/src/main.rs
+++ uefi/src/main.rs
@@ -1,11 +1,31 @@
 #![no_std]
 #![no_main]
 
+use uefi::boot::*;
 use uefi::prelude::*;
+
+use uefi::print;
 
 #[entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
-    uefi::println!("Requiem loading...");
+    print!("Requiem loading...");
+
+    let pages_result = allocate_pages(
+        AllocateType::AnyPages,
+        MemoryType::RUNTIME_SERVICES_DATA,
+        1024,
+    );
+
+    let pages = match pages_result {
+        Ok(page) => page,
+        Err(error) => {
+            print!("{}", error.status());
+            return error.status();
+        }
+    };
+
+    print!("Pointer to start of allocated chunk: {:#?}", pages);
+
     Status::SUCCESS
 }
